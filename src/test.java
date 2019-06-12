8934b7b502367a7a25e0731a379fe8e8fcacb45e
Dsky149 <liang149149@gmail.com>
2019-06-12 17:32:48 +0800

    Second

---
+++ src/test.java
@@ -0,0 +1,10 @@
+public class test {
+
+    public  static  void  main(String[] args){
+        String str = "123456789";
+        str = str.substring(1,3);
+        System.out.printf("str-----"+str);
+
+
+    }
+}
